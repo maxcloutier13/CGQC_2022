bc76aea7ec825138e8a48b4a3de128b3b7954843
Max Cloutier <mcloutier13@gmail.com>
2022-08-27 19:33:49 -0400

    Feat: Mk1 helmet camo variants

--- cfg_helmets.cpp
+++ cfg_helmets.cpp
@@ -2,34 +2,181 @@ class H_mas_can_opscorn;
 class rhsusf_cvc_green_ess;
 class H_PilotHelmetFighter_B;
 
+// Base green helmet
 class cgqc_helmet_mk1 : H_mas_can_opscorn
 {
     author = "silent1";
     _generalMacro = "cgqc_helmet_mk1";
     displayName = "CGQC Casque mk1";
     picture = "\mas_can_lite\ui\green_sf.paa";
-    
+
     hiddenSelectionsTextures[] = {
-        "\mas_can_lite\fast\data\vdo_opscore_green_co.paa",
-        "\cgqc_2022\helmets\cgqc_helmet_mk1_nylon.paa",
-        "\mas_can_lite\fast\data\ops_co.paa"};
+        "\cgqc_2022\helmets\green.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro.paa",
+        "\cgqc_2022\helmets\inside.paa"};
     // Grad helmet compatibility
     grad_slingHelmet_allow = true;
     // Ctab camera compat
     CTAB_Camera = true;
 };
-
 class cgqc_helmet_mk1_medic : cgqc_helmet_mk1
 {
     author = "silent1";
     _generalMacro = "cgqc_helmet_mk1_medic";
     displayName = "CGQC Casque Medic mk1";
     hiddenSelectionsTextures[] = {
-        "\mas_can_lite\fast\data\vdo_opscore_green_co.paa",
-        "\cgqc_2022\helmets\cgqc_helmet_mk1_medic_nylon.paa",
-        "\mas_can_lite\fast\data\ops_co.paa"};
+        "\cgqc_2022\helmets\green.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_medic.paa",
+        "\cgqc_2022\helmets\inside.paa"};
 };
-
+// Black
+class cgqc_helmet_mk1_black : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_black";
+    displayName = "CGQC Casque mk1 - Noir";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\black.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_black.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+class cgqc_helmet_mk1_black_medic : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_medic";
+    displayName = "CGQC Casque Medic mk1 - Noir";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\black.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_black_medic.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+// White
+class cgqc_helmet_mk1_white : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_white";
+    displayName = "CGQC Casque mk1 - Blanc";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\white.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_white.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+class cgqc_helmet_mk1_white_medic : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_white_medic";
+    displayName = "CGQC Casque Medic mk1 - Blanc";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\white.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_white_medic.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+// Cadpat
+class cgqc_helmet_mk1_cadpat : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_cadpat";
+    displayName = "CGQC Casque mk1 - Cadpat";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\cadpat.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+class cgqc_helmet_mk1_cadpat_medic : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_cadpat_medic";
+    displayName = "CGQC Casque Medic mk1 - Cadpat";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\cadpat.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_medic.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+// Coyote
+class cgqc_helmet_mk1_coyote : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_coyote";
+    displayName = "CGQC Casque mk1 - Coyote";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\coyote.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+class cgqc_helmet_mk1_coyote_medic : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_coyote_medic";
+    displayName = "CGQC Casque Medic mk1 - Coyote";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\coyote.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_medic.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+// Desert
+class cgqc_helmet_mk1_desert : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_desert";
+    displayName = "CGQC Casque mk1 - Desert";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\desert.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+class cgqc_helmet_mk1_desert_medic : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_desert_medic";
+    displayName = "CGQC Casque Medic mk1 - Desert";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\desert.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_medic.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+// Multicam
+class cgqc_helmet_mk1_mcam : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_mcam";
+    displayName = "CGQC Casque mk1 - Multicam";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\mcam.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+class cgqc_helmet_mk1_mcam_medic : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_mcam_medic";
+    displayName = "CGQC Casque Medic mk1 - Multicam";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\mcam.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_medic.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+// Gray
+class cgqc_helmet_mk1_gray : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_gray";
+    displayName = "CGQC Casque mk1 - Gray";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\gray.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+class cgqc_helmet_mk1_gray_medic : cgqc_helmet_mk1
+{
+    author = "silent1";
+    _generalMacro = "cgqc_helmet_mk1_gray_medic";
+    displayName = "CGQC Casque Medic mk1 - Gray";
+    hiddenSelectionsTextures[] = {
+        "\cgqc_2022\helmets\gray.paa",
+        "\cgqc_2022\helmets\cgqc_helmet_mk1_velcro_medic.paa",
+        "\cgqc_2022\helmets\inside.paa"};
+};
+//Other helmets
 class cgqc_helmet_mk1_tank : rhsusf_cvc_green_ess
 {
     author = "silent1";
@@ -37,7 +184,6 @@ class cgqc_helmet_mk1_tank : rhsusf_cvc_green_ess
     displayName = "CGQC Casque Tank mk1";
     grad_slingHelmet_allow = true;
 };
-
 class cgqc_helmet_mk1_jetpilot : H_PilotHelmetFighter_B
 {
     author = "silent1";

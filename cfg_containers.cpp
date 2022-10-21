ec049af43a6b0cd28d0e75633eb3d0ed77aa007e
Maxime Cloutier <mcloutier13@gmail.com>
2022-10-20 20:40:39 -0400

    Ajout: Supply crate

--- cfg_containers.cpp
+++ cfg_containers.cpp
@@ -158,6 +158,238 @@ class cgqc_box_original_green : B_supplyCrate_F
     displayName = "CGQC SupplyBox Originale (green)";
     hiddenSelectionsTextures[] = {"\CGQC_2022\containers\cgqc_box_originale_green.paa"};
 };
+
+// Mk1 Ressuply box
+class cgqc_box_mk1_supplies : cgqc_box_mk1_rolebox
+{
+    author = "silent1";
+    _generalMacro = "cgqc_box_mk1_supplies";
+    displayName = "CGQC MK1 Supplies";
+    hiddenSelectionsTextures[] = {"\CGQC_2022\containers\cgqc_box_mk1_rolebox_green.paa",
+                                  "\CGQC_2022\containers\cgqc_box_mk1_supplies.paa"};
+    class TransportMagazines
+    {
+        class _xx_ACE_M84
+        {
+            count = 20;
+            magazine = "ACE_M84";
+        };
+        class _xx_HandGrenade
+        {
+            count = 20;
+            magazine = "HandGrenade";
+        };
+        class _xx_ACE_Chemlight_HiRed
+        {
+            count = 20;
+            magazine = "ACE_Chemlight_HiRed";
+        };
+        class _xx_ACE_Chemlight_IR
+        {
+            count = 20;
+            magazine = "ACE_Chemlight_IR";
+        };
+        class _xx_SmokeShell
+        {
+            count = 20;
+            magazine = "SmokeShell";
+        };
+        class _xx_SmokeShellBlue
+        {
+            count = 20;
+            magazine = "SmokeShellBlue";
+        };
+        class _xx_SmokeShellRed
+        {
+            count = 20;
+            magazine = "SmokeShellRed";
+        };
+        class _xx_B_IR_Grenade
+        {
+            count = 20;
+            magazine = "B_IR_Grenade";
+        };
+        class _xx_1Rnd_HE_Grenade_shell
+        {
+            count = 20;
+            magazine = "1Rnd_HE_Grenade_shell";
+        };
+        class _xx_UGL_FlareRed_F
+        {
+            count = 20;
+            magazine = "UGL_FlareRed_F";
+        };
+        class _xx_1Rnd_SmokeBlue_Grenade_shell
+        {
+            count = 20;
+            magazine = "1Rnd_SmokeBlue_Grenade_shell";
+        };
+        class _xx_1Rnd_Smoke_Grenade_shell
+        {
+            count = 20;
+            magazine = "1Rnd_Smoke_Grenade_shell";
+        };
+        class _xx_Laserbatteries
+        {
+            count = 4;
+            magazine = "Laserbatteries";
+        };
+        class _xx_rhsusf_m112_mag
+        {
+            count = 10;
+            magazine = "rhsusf_m112_mag";
+        };
+        class _xx_rhsusf_m112x4_mag
+        {
+            count = 10;
+            magazine = "rhsusf_m112x4_mag";
+        };
+
+        class _xx_rhsusf_mine_m14_mag
+        {
+            count = 20;
+            magazine = "rhsusf_mine_m14_mag";
+        };
+        // Pistol
+        
+        class _xx_Tier1_15Rnd_9x19_JHP
+        {
+            count = 16;
+            magazine = "Tier1_15Rnd_9x19_JHP";
+        };
+        // 556
+        class _xx_rhs_mag_30Rnd_556x45_Mk262_Stanag_Pull
+        {
+            count = 50;
+            magazine = "rhs_mag_30Rnd_556x45_Mk262_Stanag_Pull";
+        };
+        // 762
+        class _xx_ACE_20Rnd_762x51_Mk319_Mod_0_Mag
+        {
+            count = 40;
+            magazine = "ACE_20Rnd_762x51_Mk319_Mod_0_Mag";
+        };
+        // Sniper
+        class _xx_7Rnd_408_Mag
+        {
+            count = 25;
+            magazine = "7Rnd_408_Mag";
+        };
+        // Driver/Pilot
+        class _xx_50Rnd_570x28_SMG_03
+        {
+            count = 12;
+            magazine = "50Rnd_570x28_SMG_03";
+        };
+        // Javelin
+        class _xx_jav_AT_mas_can
+        {
+            count = 10;
+            magazine = "jav_AT_mas_can";
+        };
+        // Maaws
+        class _xx_MRAWS_HEAT_F
+        {
+            count = 10;
+            magazine = "MRAWS_HEAT_F";
+        };
+        // lmg
+        class _xx_rhsusf_200Rnd_556x45_mixed_soft_pouch_coyote
+        {
+            count = 20;
+            magazine = "rhsusf_200Rnd_556x45_mixed_soft_pouch_coyote";
+        };
+        // hmg
+        class _xx_Tier1_250Rnd_762x51_Belt_M993_AP
+        {
+            count = 20;
+            magazine = "Tier1_250Rnd_762x51_Belt_M993_AP";
+        };
+
+    };
+    class TransportWeapons
+    {
+        
+    };
+    class TransportItems
+    {
+        // Radios  ----------------------------------
+        class _xx_ACRE_PRC343
+        {
+            count = 2;
+            name = "ACRE_PRC343";
+        };
+        class _xx_ACRE_PRC152
+        {
+            count = 2;
+            name = "ACRE_PRC152";
+        };
+        // Medical  ----------------------------------
+        class _xx_ACE_fieldDressing
+        {
+            count = 50;
+            name = "ACE_fieldDressing";
+        };
+        class _xx_ACE_salineIV
+        {
+            count = 10;
+            name = "ACE_salineIV";
+        };
+        class _xx_ACE_salineIV_500
+        {
+            count = 10;
+            name = "ACE_salineIV_500";
+        };
+        class _xx_FF_Painkiller
+        {
+            count = 30;
+            name = "FF_Painkiller";
+        };
+        class _xx_ACE_morphine
+        {
+            count = 20;
+            name = "ACE_morphine";
+        };
+        class _xx_ACE_epinephrine
+        {
+            count = 20;
+            name = "ACE_epinephrine";
+        };
+        class _xx_ACE_splint
+        {
+            count = 5;
+            name = "ACE_splint";
+        };
+        class _xx_ACE_tourniquet
+        {
+            count = 5;
+            name = "ACE_tourniquet";
+        };
+        // Items ------------------------
+        class _xx_ACE_EarPlugs
+        {
+            count = 5;
+            name = "ACE_EarPlugs";
+        };
+        class _xx_ACE_UAVBattery
+        {
+            count = 5;
+            name = "ACE_UAVBattery";
+        };
+        class _xx_ACE_DefusalKit
+        {
+            count = 2;
+            name = "ACE_DefusalKit";
+        };
+        class _xx_ACE_M26_Clacker
+        {
+            count = 2;
+            name = "ACE_M26_Clacker";
+        };
+    };
+};
+
+
 // Basic Backpacks --------------------------------------------------------------------------------------
 class cgqc_pack_mk1_kitbag : B_Kitbag_rgr // Base
 {

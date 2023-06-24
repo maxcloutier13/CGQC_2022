59ba8be4720d16dfb51ac1c4d117ea10c4945b60
Maxime Cloutier <mcloutier13@gmail.com>
2023-06-24 16:53:18 -0400

    Modif: Bigger uniforms

--- cfg_uniforms.cpp
+++ cfg_uniforms.cpp
@@ -7,7 +7,7 @@ class U_mas_can_B_CombatUniform_S_NTOG;
 class U_mas_can_B_CombatUniform_S_SWAT;
 class U_I_pilotCoveralls;
 class U_mas_can_B_Wetsuit;
-
+class UniformItem;
 /*
 // Base/training
 class cgqc_uniform_mk1 : U_mas_can_B_CombatUniform_S_NTOG
@@ -23,6 +23,13 @@ class cgqc_uniform_mk1_desert : U_mas_can_B_CombatUniform_aor1
     author = "silent1";
     _generalMacro = "cgqc_uniform_mk1_desert";
     displayName = "CGQC Uniforme mk1 - Desert";
+    class ItemInfo: UniformItem
+    {
+        uniformModel = "-";
+        uniformClass = "";
+        containerClass = "Supply60";
+        mass = 20;
+    };
 };
 // Multicam
 class cgqc_uniform_mk1_mcam : U_mas_can_B_CombatUniform_multi
@@ -30,6 +37,13 @@ class cgqc_uniform_mk1_mcam : U_mas_can_B_CombatUniform_multi
     author = "silent1";
     _generalMacro = "cgqc_uniform_mk1_multicam";
     displayName = "CGQC Uniforme mk1 - Multicam";
+    class ItemInfo: UniformItem
+    {
+        uniformModel = "-";
+        uniformClass = "";
+        containerClass = "Supply60";
+        mass = 20;
+    };
 };
 // Woodland
 class cgqc_uniform_mk1_cadpat : U_mas_can_B_CombatUniform_aor2
@@ -37,6 +51,13 @@ class cgqc_uniform_mk1_cadpat : U_mas_can_B_CombatUniform_aor2
     author = "silent1";
     _generalMacro = "cgqc_uniform_mk1_woodland";
     displayName = "CGQC Uniforme mk1 - Cadpat";
+    class ItemInfo: UniformItem
+    {
+        uniformModel = "-";
+        uniformClass = "";
+        containerClass = "Supply60";
+        mass = 20;
+    };
 };
 // Black
 class cgqc_uniform_mk1_black : U_mas_can_B_suite_CQB
@@ -44,6 +65,13 @@ class cgqc_uniform_mk1_black : U_mas_can_B_suite_CQB
     author = "silent1";
     _generalMacro = "cgqc_uniform_mk1_black";
     displayName = "CGQC Uniforme mk1 - Black";
+    class ItemInfo: UniformItem
+    {
+        uniformModel = "-";
+        uniformClass = "";
+        containerClass = "Supply60";
+        mass = 20;
+    };
 };
 // Winter
 class cgqc_uniform_mk1_white : U_mas_can_B_suite_WINT
@@ -51,6 +79,13 @@ class cgqc_uniform_mk1_white : U_mas_can_B_suite_WINT
     author = "silent1";
     _generalMacro = "cgqc_uniform_mk1_winter";
     displayName = "CGQC Uniforme mk1 - Winter";
+    class ItemInfo: UniformItem
+    {
+        uniformModel = "-";
+        uniformClass = "";
+        containerClass = "Supply60";
+        mass = 20;
+    };
 };
 
 // Pilots
@@ -59,12 +94,26 @@ class cgqc_uniform_mk1_jetpilot : U_I_pilotCoveralls
     author = "silent1";
     _generalMacro = "cgqc_uniform_jetpilot";
     displayName = "CGQC Pilote de jet mk1";
+    class ItemInfo: UniformItem
+    {
+        uniformModel = "-";
+        uniformClass = "";
+        containerClass = "Supply60";
+        mass = 20;
+    };
 };
 class cgqc_uniform_mk1_helipilot : U_mas_can_B_CombatUniform_S_SWAT
 {
     author = "silent1";
     _generalMacro = "cgqc_uniform_mk1_jetpilot";
     displayName = "CGQC Pilote Hélicoptère mk1";
+    class ItemInfo: UniformItem
+    {
+        uniformModel = "-";
+        uniformClass = "";
+        containerClass = "Supply60";
+        mass = 20;
+    };
 };
 
 // Diver
@@ -73,4 +122,11 @@ class cgqc_uniform_mk1_diver : U_mas_can_B_Wetsuit
     author = "silent1";
     _generalMacro = "cgqc_uniform_mk1_diver";
     displayName = "CGQC Plongeur mk1";
+    class ItemInfo: UniformItem
+    {
+        uniformModel = "-";
+        uniformClass = "";
+        containerClass = "Supply60";
+        mass = 20;
+    };
 };
